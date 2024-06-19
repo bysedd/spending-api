e02f06576ea821a2b1b66004d00e69aebe25bbed
Felippe <4drade@gmail.com>
2024-06-19 12:09:59 -0300

    feat(UserService): implement CRUD operations for UserService

--- src/main/java/com/example/spending/domain/service/UserService.java
+++ src/main/java/com/example/spending/domain/service/UserService.java
@@ -1,34 +1,94 @@
 package com.example.spending.domain.service;
 
+import com.example.spending.domain.exception.ResourceBadRequestException;
+import com.example.spending.domain.exception.ResourceNotFoundException;
+import com.example.spending.domain.model.User;
+import com.example.spending.domain.repository.UserRepository;
 import com.example.spending.dto.user.UserRequestDto;
 import com.example.spending.dto.user.UserResponseDto;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Service
+@AllArgsConstructor
 public class UserService implements ICRUDService<UserRequestDto, UserResponseDto> {
+
+    private final UserRepository userRepository;
+
+    private final ModelMapper mapper;
 
     @Override
     public List<UserResponseDto> findAll() {
-        return List.of();
+        List<User> users = userRepository.findAll();
+
+        return users.stream()
+                .map(user -> mapper.map(user, UserResponseDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserResponseDto findById(Long id) {
-        return null;
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isEmpty()) {
+            throw new ResourceNotFoundException("Unable to find user with id: " + id);
+        }
+
+        return mapper.map(user.get(), UserResponseDto.class);
     }
 
     @Override
     public UserResponseDto register(UserRequestDto dto) {
-        return null;
+        validateUser(dto);
+
+        User user = mapper.map(dto, User.class);
+        user.setId(null);
+        user.setRegisterDate(new Date());
+        // TODO: Encrypt password
+
+        user = userRepository.save(user);
+        return mapper.map(user, UserResponseDto.class);
     }
 
     @Override
     public UserResponseDto update(Long id, UserRequestDto dto) {
-        return null;
+        UserResponseDto bankUser = findById(id);
+        validateUser(dto);
+
+        User user = mapper.map(dto, User.class);
+        // TODO: Encrypt password
+
+        user.setId(id);
+        user.setInactivationDate(bankUser.getInactivationDate());
+        user.setRegisterDate(bankUser.getRegisterDate());
+
+        user = userRepository.save(user);
+        return mapper.map(user, UserResponseDto.class);
     }
 
     @Override
     public void delete(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
 
+        if (optionalUser.isEmpty()) {
+            throw new ResourceNotFoundException("Unable to find user with id: " + id);
+        }
+
+        User user = optionalUser.get();
+        user.setInactivationDate(new Date());
+
+        userRepository.save(user);
+    }
+
+    private void validateUser(UserRequestDto dto) {
+        if (dto.getEmail() == null || dto.getPassword() == null) {
+            throw new ResourceBadRequestException("Email and password are required");
+        }
     }
 }
