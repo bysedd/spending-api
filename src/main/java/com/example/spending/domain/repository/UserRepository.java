16a54458cbc8f92b5d401d5f2603baaa10e8aeef
Felippe <4drade@gmail.com>
2024-06-19 14:40:56 -0300

    refactor(UserRepository): change findByEmail return type to Optional

--- src/main/java/com/example/spending/domain/repository/UserRepository.java
+++ src/main/java/com/example/spending/domain/repository/UserRepository.java
@@ -3,9 +3,9 @@ package com.example.spending.domain.repository;
 import com.example.spending.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
