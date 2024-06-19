da21c765fee6c50d992bc82fcfd382f4b9c22dc0
Felippe <4drade@gmail.com>
2024-06-19 12:09:47 -0300

    feat: add ResourceBadRequestException and ResourceNotFoundException classes

---
+++ src/main/java/com/example/spending/domain/exception/ResourceBadRequestException.java
@@ -0,0 +1,8 @@
+package com.example.spending.domain.exception;
+
+public class ResourceBadRequestException extends RuntimeException {
+
+    public ResourceBadRequestException(String message) {
+        super(message);
+    }
+}
