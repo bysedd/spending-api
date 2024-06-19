ff29cf5c1cae7d1ccaca5d6b3fff3f42e3fd8a82
Felippe <4drade@gmail.com>
2024-06-19 17:23:19 -0300

    feat(user): add LoginRequestDto and LoginResponseDto classes

---
+++ src/main/java/com/example/spending/dto/user/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.spending.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponseDto {
+
+    private String token;
+
+    private UserResponseDto user;
+}
