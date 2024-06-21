1d9b4a8e2567462db2cb58c360c31bd80fd29e94
Felippe <4drade@gmail.com>
2024-06-21 16:14:56 -0300

    refactor: reformat and rearrange components in WebSecurityConfig.java

--- src/main/java/com/example/spending/security/WebSecurityConfig.java
+++ src/main/java/com/example/spending/security/WebSecurityConfig.java
@@ -19,9 +19,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @AllArgsConstructor
 public class WebSecurityConfig {
 
-  private final UserDetailsSecurityServer userService;
-  private final JwtUtil jwtUtil;
-  private final AuthenticationConfiguration authenticationConfiguration;
+  private UserDetailsSecurityServer userService;
+
+  private JwtUtil jwtUtil;
+
+  private AuthenticationConfiguration authenticationConfiguration;
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
@@ -37,21 +39,15 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(
-                    HeadersConfigurer.FrameOptionsConfig::deny))
-            .cors(AbstractHttpConfigurer::disable)
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(
-                    authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.POST,
-                                    "/api/users")
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated())
-            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS));
+            HeadersConfigurer.FrameOptionsConfig::deny)).cors(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+            authorizeRequests -> authorizeRequests.requestMatchers(HttpMethod.POST, "/api/users")
+                .permitAll().anyRequest().authenticated()).sessionManagement(
+            sessionManagement -> sessionManagement.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
 
     http.addFilter(
-        new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration),
-            jwtUtil));
+        new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtUtil));
     http.addFilter(
         new JwtAuthorizationFilter(authenticationManager(authenticationConfiguration), jwtUtil,
             userService));
