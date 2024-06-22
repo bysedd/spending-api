67e117d34d321b69b53d4754773f0187f3bbf2e4
Felippe <4drade@gmail.com>
2024-06-22 17:55:57 -0300

    refactor: update UserController to implement ICRUDController interface

--- src/main/java/com/example/spending/controller/UserController.java
+++ src/main/java/com/example/spending/controller/UserController.java
@@ -9,11 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,35 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
-public class UserController {
+public class UserController implements ICRUDController<UserRequestDto, UserResponseDto> {
 
-  private final UserService userService;
+  private UserService userService;
 
-  @GetMapping
+  @Override
   public ResponseEntity<List<UserResponseDto>> getAll() {
     return ResponseEntity.ok(userService.getAll());
   }
 
-  @GetMapping("/{id}")
+  @Override
   public ResponseEntity<List<UserResponseDto>> getById(@PathVariable Long id) {
     return ResponseEntity.ok(Collections.singletonList(userService.getById(id)));
   }
 
-  @PostMapping
+  @Override
   public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto dto) {
     UserResponseDto user = userService.create(dto);
     return new ResponseEntity<>(user, HttpStatus.CREATED);
   }
 
-  @PutMapping("/{id}")
+  @Override
   public ResponseEntity<UserResponseDto> update(
       @PathVariable Long id, @RequestBody UserRequestDto dto) {
     UserResponseDto user = userService.update(id, dto);
     return new ResponseEntity<>(user, HttpStatus.OK);
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id) {
+  @Override
+  public ResponseEntity<?> delete(@PathVariable Long id) {
     userService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
