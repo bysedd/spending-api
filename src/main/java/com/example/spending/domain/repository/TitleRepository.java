6583e7ba41d1890e21718412f5b28d3baaa754d0
Felippe <4drade@gmail.com>
2024-06-22 22:09:17 -0300

    feat: add TitleRepository in a domain model

---
+++ src/main/java/com/example/spending/domain/repository/TitleRepository.java
@@ -0,0 +1,8 @@
+package com.example.spending.domain.repository;
+
+import com.example.spending.domain.model.Title;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TitleRepository extends JpaRepository<Title, Long> {
+
+}
